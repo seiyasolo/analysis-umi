3390a79459884fa3098275cc8987a33ccbd43060
seiyasolo <seiyasolo@gmail.com>
2019-08-07 17:48:32 +0800

    first commit

---
+++ .umirc.ts
@@ -0,0 +1,33 @@
+import { IConfig } from 'umi-types';
+import darkTheme from '@ant-design/dark-theme';
+
+// ref: https://umijs.org/config/
+const config: IConfig = {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'analysis-umi',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  theme: darkTheme
+}
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.css';
+import { formatMessage } from 'umi-plugin-locale';
+import { Input } from 'antd';
+
+export default function () {
+  return (
+    <div>
+      <Input placeholder={'fsad'}></Input>
+    </div>
+  );
+}
